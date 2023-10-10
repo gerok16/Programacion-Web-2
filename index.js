0c9e42062998140484284eb5752dd3f831549d8c
gerok16 <132413903+gerok16@users.noreply.github.com>
2023-10-10 01:43:29 +0000

    actual index.js

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-const suma = (a, b) => a + b;
+/*
+Varias funciones en JS
+*/
 
-console.log(suma (6, 7));
+function suma(a, b) {
+    return a + b
+}
+
+const resta = function(a, b) {
+    return a - b
+}
+
+const producto = (a, b) => a * b
+
+const nombreCompleto = (nombre) => `Hola, cómo estas ${nombre}`
+
+const resultado_suma = suma(2, 8)
+const resultado_resta = resta(1, 5)
+const resultado_producto = producto(3, 6)
+
+console.log(resultado_suma)
+console.log(resultado_resta)
+console.log(resultado_producto)
+console.log(nombreCompleto('Carlos'));
